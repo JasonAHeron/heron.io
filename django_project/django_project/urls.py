107db379c83ade80d43d4830bff41d43ec66fd39
Jason Heron <jasonaheron@gmail.com>
2014-11-10 01:53:09 -0500

    doing what i meant to do in my last commit

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from heron import views as v
 
 admin.autodiscover()
 
