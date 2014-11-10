7aa0e653a8d356cce7ffb3192927ba4c9629e977
Jason Heron <JasonAHeron@Gmail.com>
2014-11-09 22:40:45 -0800

    adding bootstrap theme

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', v.IndexView.as_view()),
 )
--- django_project/heron/views.py
+++ django_project/heron/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login
+from django.views.generic import View, CreateView
+from django.core.urlresolvers import reverse
+from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
 
-# Create your views here.
+class IndexView(TemplateView):
+  template_name = 'index.html'
