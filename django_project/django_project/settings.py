850be9daed506b98db5f0707ea413a541e9e1ba5
Jason Heron <jasonaheron@gmail.com>
2014-11-10 01:04:46 -0500

    adding heron.io project

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -66,6 +66,17 @@ DATABASES = {
     }
 }
 
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
