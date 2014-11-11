8166a7b2d34673057c6f198e979cda815d16088b
Jason Heron <JasonAHeron@Gmail.com>
2014-11-10 19:29:01 -0800

    adding resume

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -82,7 +82,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-'''
+
 
 
 
